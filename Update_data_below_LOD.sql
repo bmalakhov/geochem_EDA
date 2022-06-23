b021907b14c58cfe8897962767b56f38a1848659
bm <bmalakhov@gmail.com>
2022-06-23 13:43:17 +0300

    Add files via upload

---
+++ Update_data_below_LOD.sql
@@ -0,0 +1,23 @@
+﻿/*View data below current method LOD*/
+select count(t1.id)
+--select t1.id, t3.id, t3.name, t1.mant, t2.detection_limit, t1.exclusion_ref, t1.expo
+from data t1, detection_limits t2 , elements t3
+where t1.element_ref=t2.element_ref and t2.method_ref=0 and t1.element_ref=t3.id 
+--and t3.name='Ta' 
+and t1.mant<t2.detection_limit 
+--and t1.mant>0 
+and t1.exclusion_ref=2
+order by t1.mant asc
+
+/*Update data below LOD it will be marked exclusion_ref=2  */
+update data 
+set exclusion_ref=2 
+where data.exclusion_ref=0 and data.id in (
+select t1.id
+from data t1, detection_limits t2 , elements t3
+where t1.element_ref=t2.element_ref and t2.method_ref=0 and t1.element_ref=t3.id 
+--and t3.name='Ta' 
+and t1.mant<t2.detection_limit 
+--and t1.mant>0 
+and t1.exclusion_ref=0
+)
---
+++ get_clear_data.sql
@@ -0,0 +1,29 @@
+/*Example sql to extract all data from Scott Halley's geochemistry dataset*/
+select 
+       t1.id,
+       t3.id as "sample id", 
+       t3.east,
+       t3.north,
+       t3.elevation,
+       t2.name as "hole",
+       t3.from1 as "from",
+       t3.to1 as "to",
+       t4.name as "element",
+       t1.mant,  /*mantissa value*/
+       t1.expo, /*exponent -2 pct and -6 ppm*/
+       t1.exclusion_ref,
+       t5.name   as "exclusion reasons"
+from 
+     data t1, /*data table*/
+     holes t2,  /*holes table*/
+     samples t3, /*samles table*/
+     elements t4, /*elements table*/
+     exclusion_reasons t5 /*exlusion reasons table*/
+where 
+      t1.sample_ref=t3.id  /* join samples and data */
+      and t3.hole_ref=t2.id   /* join samples and holes */
+      and t1.element_ref=t4.id   /* join elements and data */
+      and t1.exclusion_ref=t5.id /* join data and exclusion_reasons */
+      /*if label exclusion_ref=0 then only unequivocal data will be retrieved, exclusion_ref<>0 to view exclusions or remove next line if you need all */
+      /*and t1.exclusion_ref=0 */
+order by t3.id, t4.name asc 
